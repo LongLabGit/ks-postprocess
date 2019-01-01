67af2f43aec13a28fd5cec929cd0059718957ece
robert <robert.egger@nyumc.org>
2018-12-31 19:21:25 -0800

    version ready for use in KiloSort pipeline

--- extract_spike_waveforms.py
+++ extract_spike_waveforms.py
@@ -8,6 +8,7 @@ import os.path
 from recordingio import load_recording
 from scipy import signal
 from tqdm import tqdm
+import argparse
 
 # -----------------------------------------------------------------------------
 # Extract waveforms
@@ -15,10 +16,10 @@ from tqdm import tqdm
 
 
 # some default variables
-recording_name = 'amplifier_chunk.dat'
-extract_wf_name = 'extracted_wf_chunk.dat'
-spiketimes_name = 'spike_times_chunk.npy'
-spikeclusters_name = 'spike_clusters_chunk.npy'
+recording_name = 'amplifier.dat'
+extract_wf_name = 'extracted_wf.dat'
+spiketimes_name = 'spike_times.npy'
+spikeclusters_name = 'spike_clusters.npy'
 templates_name = 'templates.npy'
 channel_map_name = 'channel_map.npy'
 channel_shank_map_name = 'channel_shank_map.npy'
@@ -66,8 +67,8 @@ def extract_cluster_waveforms(folder, nchannels, fs, wf_samples=61, dtype=np.dty
     :return: None
     """
     # load stuff
-    spike_times = np.load(os.path.join(folder, spiketimes_name))
-    spike_clusters = np.load(os.path.join(folder, spikeclusters_name))
+    spike_times = np.load(os.path.join(folder, spiketimes_name)).flatten()
+    spike_clusters = np.load(os.path.join(folder, spikeclusters_name)).flatten()
     templates = np.load(os.path.join(folder, templates_name))
     channel_map = np.load(os.path.join(folder, channel_map_name))
     channel_shank_map = np.load(os.path.join(folder, channel_shank_map_name))
@@ -82,6 +83,7 @@ def extract_cluster_waveforms(folder, nchannels, fs, wf_samples=61, dtype=np.dty
     extract_wf_file = np.memmap(os.path.join(folder, extract_wf_name), dtype=dtype.name, mode='w+',
                                 shape=(channels_per_shank, new_samples), order='F')
 
+    # load recording data file
     rec_file = load_recording(os.path.join(folder, recording_name), nchannels)
     rec_file_info = os.stat(os.path.join(folder, recording_name))
     rec_file_size = rec_file_info.st_size
@@ -96,21 +98,56 @@ def extract_cluster_waveforms(folder, nchannels, fs, wf_samples=61, dtype=np.dty
     for i, spike_sample in enumerate(tqdm(spike_times)):
         spike_cluster = spike_clusters[i]
         wf = np.zeros((channels_per_shank, wf_samples), dtype=dtype.name, order='F')
-        # carefule with the edge cases - zero-padding
-        start_index_ = np.int64(spike_sample) - wf_offset # uint64 converted silently to float64 when adding an int
-        start_index, start_diff = (start_index_, 0) if start_index_ >= 0 else (0, -start_index_)
-        stop_index_ = np.int64(spike_sample) + wf_offset + 1 # uint64 converted silently to float64 when adding an int
-        stop_index, stop_diff = (stop_index_, 0) if stop_index_ < rec_samples else (rec_samples, rec_samples - stop_index_)
+        # careful with the edge cases - zero-padding
+        # uint64 converted silently to float64 when adding an int - cast to int64
+        start_index_ = np.int64(spike_sample) - wf_offset
+        start_index, start_diff = (start_index_, 0) if start_index_ >= 0 \
+                                else (0, -start_index_)
+        # uint64 converted silently to float64 when adding an int - cast to int64
+        stop_index_ = np.int64(spike_sample) + wf_offset + 1
+        stop_index, stop_diff = (stop_index_, 0) if stop_index_ < rec_samples \
+                            else (rec_samples, rec_samples - stop_index_)
         # now copy the appropriately sized snippet from channels on same clusters
-        wf[:, start_diff:wf_samples-stop_diff] += rec_file[cluster_channels[spike_cluster].flatten(), start_index:stop_index]
-        # wf = bp_filter(wf)
+        wf[:, start_diff:wf_samples-stop_diff] += rec_file[cluster_channels[spike_cluster].flatten(),
+                                                  start_index:stop_index]
+        # wf = bp_filter(wf) # takes about twice as long to filter every spike
         extract_wf_file[:, i*wf_samples:(i+1)*wf_samples] = wf
 
     extract_wf_file.flush()
 
 
-folder = r'/Volumes/Time Machine Backups/UCLAProbeTest'
-nchannels = 128
-fs = 3e4
-wf_samples = 45
-extract_cluster_waveforms(folder, nchannels, fs, wf_samples)
+# command line usage
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', required=True, nargs='+')
+    parser.add_argument('--nchan', required=True, type=int)
+    parser.add_argument('--fs', type=float, default=3e4)
+    parser.add_argument('--wfsamp', type=int, default=61)
+    parser.add_argument('--dtype', default='int16')
+    parser.add_argument('--recname', nargs='+')
+    parser.add_argument('--outname', nargs='+')
+    parser.add_argument('--stname')
+    parser.add_argument('--scname')
+    parser.add_argument('--tname')
+    parser.add_argument('--chanmap')
+    parser.add_argument('--chanshmap')
+
+    args = parser.parse_args()
+
+    if args.recname:
+        recording_name = ' '.join(args.recname)
+    if args.outname:
+        extract_wf_name = ' '.join(args.outname)
+    if args.stname:
+        spiketimes_name = args.stname
+    if args.scname:
+        spikeclusters_name = args.scname
+    if args.tname:
+        templates_name = args.tname
+    if args.chanmap:
+        channel_map_name = args.chanmap
+    if args.chanshmap:
+        channel_shank_map_name = args.chanshmap
+
+    dir_ = ' '.join(args.dir)
+    extract_cluster_waveforms(dir_, args.nchan, args.fs, args.wfsamp, np.dtype(args.dtype))
